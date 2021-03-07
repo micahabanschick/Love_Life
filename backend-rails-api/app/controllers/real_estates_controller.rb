3a7815efd532fc4187e803d61ea6999a24c818d7
micahabanschick <micha.ban@gmail.com>
2021-03-07 14:33:49 -0500

    BasicController:RealEstate

--- backend-rails-api/app/controllers/real_estates_controller.rb
+++ backend-rails-api/app/controllers/real_estates_controller.rb
@@ -1,2 +1,24 @@
 class RealEstatesController < ApplicationController
+
+    def index
+        real_estates = RealEstate.all
+        render json: RealEstateSerializer.new(real_estates)
+    end
+
+    def show
+        real_estate = RealEstate.find(params[:id])
+        render json: RealEstateSerializer.new(real_estate)
+    end
+
+    def create
+        real_estate = RealEstate.find_or_create_by(address: real_estate_params[:address])
+        render json: RealEstateSerializer.new(real_estate)
+    end
+
+    private
+
+    def real_estate_params
+        params.require(:real_estate).permit(:address, :purchase_price, :units, :unit_rent)
+    end
+
 end
