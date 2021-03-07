45606c0dd28f28374af70f41b02e316be7de2765
micahabanschick <micha.ban@gmail.com>
2021-03-07 16:43:26 -0500

    frontend structure with form and fetch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,13 @@
 const BACKEND_URL = 'localhost:3000';
-fetch(`${BACKEND_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+fetch(`${BACKEND_URL}`,{
+  method:'POST',
+  headers: {
+    "Content-Type":"application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(this)
+})
+.then(resp=>resp.json())
+.then(response => response.json())
+.then(parsedResponse => console.log(parsedResponse))
+.catch(error => alert(error.message));
