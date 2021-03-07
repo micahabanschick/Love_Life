cdccb1ecf63499ff54cb9e2cd609393d4ca4dcaf
micahabanschick <micha.ban@gmail.com>
2021-03-07 14:37:24 -0500

    BasicController:Expense

--- backend-rails-api/app/controllers/expenses_controller.rb
+++ backend-rails-api/app/controllers/expenses_controller.rb
@@ -1,2 +1,24 @@
 class ExpensesController < ApplicationController
+
+    def index
+        expenses = Expense.all
+        render json: ExpenseSerializer.new(expenses)
+    end
+
+    def show
+        expense = Expense.find(params[:id])
+        render json: ExpenseSerializer.new(expense)
+    end
+
+    def create
+        expense = Expense.find_or_create_by(name: expense_params[:name])
+        render json: ExpenseSerializer.new(expense)
+    end
+
+    private
+
+    def expense_params
+        params.require(:expense).permit(:name, :price, :type)
+    end
+
 end
