ffdb7e8c0724b3dcb80e00369d30738a7216be78
micahabanschick <micha.ban@gmail.com>
2021-03-07 14:20:25 -0500

    Serialize (Updated)

--- backend-rails-api/app/serializers/expense_serializer.rb
+++ backend-rails-api/app/serializers/expense_serializer.rb
@@ -1,4 +1,6 @@
 class ExpenseSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :price, :type
+  belongs_to :real_estate 
+  belongs_to :user
 end
--- backend-rails-api/app/serializers/real_estate_serializer.rb
+++ backend-rails-api/app/serializers/real_estate_serializer.rb
@@ -2,4 +2,5 @@ class RealEstateSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :address, :purchase_price, :units, :unit_rent
   has_many :expenses
+  belongs_to :user
 end
