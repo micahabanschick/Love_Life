ff33837bab16587eaa27c25fcbcb018d7f0c6e4d
micahabanschick <micha.ban@gmail.com>
2021-03-07 14:28:27 -0500

    BasicController:User

--- backend-rails-api/app/controllers/users_controller.rb
+++ backend-rails-api/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users)
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: UserSerializer.new(user)
+    end
+
+    def create
+        user = User.find_or_create_by(name: user_params[:name])
+        render json: UserSerializer.new(user)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :monthly_income)
+    end
+
 end
