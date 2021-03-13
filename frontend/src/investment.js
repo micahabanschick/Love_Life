923fb5fda9e817087d4939ee0bc5407d374d7e8f
micahabanschick <micha.ban@gmail.com>
2021-03-13 14:43:22 -0500

    Class:Necessity/Luxury/Investment(Initial)

---
+++ frontend/src/investment.js
@@ -0,0 +1,7 @@
+class Investment extends Expense {
+
+    constructor(name, price, category) {
+        super(name, price, category)
+    }
+    
+};
---
+++ frontend/src/necessity.js
@@ -0,0 +1,7 @@
+class Need extends Expense {
+
+    constructor(name, price, category) {
+        super(name, price, category)
+    }
+    
+};
