3aa82bef6a3e9f8a17fa04bd46a298efcb7bae62
micahabanschick <micha.ban@gmail.com>
2021-04-10 22:09:20 -0400

    create-react-app client --template redux

---
+++ v2/old_client/src/index.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  
+  let body = document.querySelector('body'); 
+  body.style.backgroundColor = "red";
+  let mainUrl = new Backend();
+  mainUrl.basicFetch('get', 'users');
+  User.displayLogin();
+  
+  // const guy = new User("guy", "password", 10000);
+  
+  // Frontend.renderPortfolio(guy);
+  
+  document.querySelector('#dark-mode').addEventListener('click', event => {
+    body = document.querySelector('body'); 
+    // background-image: linear-gradient(to bottom right, rgb(97, 0, 97), rgb(107, 0, 0));
+    if (body.style.backgroundColor === "red") {
+      body.style.backgroundColor = "blue";
+      console.log(body.style.backgroundColor)
+    } else if (body.style.backgroundColor === "blue") {
+      body.style.backgroundColor = "red";
+      console.log(body.style.backgroundColor)
+    }
+  })
+  
+  document.getElementById('login-form').addEventListener('submit', function(event) {
+    
+    console.log(this.parentElement)
+    let name = document.getElementById('user_name').value
+    let password = document.getElementById('password').value
+    let user = new User(name, password);
+    let url = new Backend();
+    url.basicFetch('post', 'users', user, resp => {
+      user.id = parseInt(resp.data.id);
+    });
+    this.parentElement.innerHTML+=`<label id="logged-in-user">Welcome ${user.name}</label><br>`
+    User.removeLogin();
+    User.displayLogout();
+    Frontend.renderPortfolio(user); 
+    event.preventDefault(); 
+
+    // User.return(resp => {
+    //   let user = new User(resp.name, resp.password, resp.monthlyIncome);
+    //   console.log(user.name);
+    // });     
+    console.log(user.name)
+
+    
+    document.querySelector('#logout-button').addEventListener('click', function(event) {
+      // mainUrl.basicFetch('post', 'users', {logout: true});
+      Frontend.removePorfolio();
+      console.log('logged out');
+      window.location.reload(true);
+    });
+    
+    document.querySelectorAll('.items').forEach(item => {
+
+      item.addEventListener('click', function(event) {
+        if(item.style.color === "gold") {
+          item.setAttribute("style", "color: white;");
+          // Expense.removeForm(item);
+        } else {
+          item.setAttribute("style", "color: gold");
+          Expense.addForm(item);
+          // item.querySelector('.expense-form input[type="submit"]').disabled = true;
+          item.querySelector('.expense-form').addEventListener('submit', function(event) {
+            console.log(this.parentElement);
+            let category = item.closest('ul').id;
+            let index = Array.prototype.indexOf.call(item.closest('ul').children, item.parentNode);
+            let price = parseInt(item.querySelector('.price').value);
+            let expense = new Expense(category, index, price);
+            expense.add(user);
+            url.basicFetch('post', 'expenses', expense);
+            url.basicFetch('get', 'expenses');
+            console.log(user.expenses())
+            console.log(user.monthlyIncome);
+            console.log(user.totalNecessities);
+            console.log(user.totalLuxuries);
+            console.log(user.totalInvestments);
+            Frontend.resize(user);
+            Expense.removeForm(item);
+            event.preventDefault();
+            // item.setAttribute("style", "color: white;");
+          });
+        }
+      });
+
+    });
+
+
+  });
+        
+  document.addEventListener('reload', function(event) {
+    event.preventDefault();
+  });
+  
+  
+});
