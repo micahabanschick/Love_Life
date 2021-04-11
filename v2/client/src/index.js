8a6363411b378a6149ef66eeee3bfebcf8002f3a
micahabanschick <micha.ban@gmail.com>
2021-04-10 22:49:33 -0400

    imports (BrowserRouter, thunk, combineReducers, compose, applyMiddleware)

--- v2/client/src/index.js
+++ v2/client/src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import { store } from './store';
-import { Provider } from 'react-redux';
+// import { store } from './store';
+// import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+
+const rootReducer = combineReducers({
+  // user: usersReducer,
+  // project: projectsReducer,
+  // work: worksReducer,
+})
+
+const store = createStore(
+  rootReducer, 
+  // initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
